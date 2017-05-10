46a6d82b8a0aeb439e962a4ddef7097042013fcb
Francesco <micheli@cern.ch>
2017-05-10 21:34:41 +0200

    small fix

--- scripts/setup_scikit.sh
+++ scripts/setup_scikit.sh
@@ -1,5 +1,5 @@
 #env setup for t3
 export PATH=/swshare/anaconda/bin:$PATH
 source /cvmfs/cms.cern.ch/slc6_amd64_gcc493/external/gcc/4.9.3/etc/profile.d/init.sh
-source /cvmfs/cms.cern.ch/slc6_amd64_gcc493/lcg/root/6.06.04/bin/thisroot.shexport 
+source /cvmfs/cms.cern.ch/slc6_amd64_gcc493/lcg/root/6.06.04/bin/thisroot.sh
 export PYTHONPATH=$PYTHONPATH:~/HHbbgg_ETH/Training/python/
