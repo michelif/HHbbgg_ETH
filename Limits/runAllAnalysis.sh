f4fd354386cdebc0b39c18cf69fd70a8b1c9fc2e
Francesco <micheli@cern.ch>
2018-01-09 17:18:38 +0100

    first commit

---
+++ Limits/runAllAnalysis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+ntup="20170620"
+addHHTagger=0
+training="allMC_resWeighting_F_noDR_minDRGJet"
+outTag="20180108_test" 
+
+while getopts ":n:at:o:" opt; do
+    case "${opt}" in
+    n)  ntup=${OPTARG}
+        ;;
+    a)  addHHTagger=1
+        ;;
+    t)  training=${OPTARG}
+        ;;
+    o)  outTag=${OPTARG}
+        ;;
+    
+    esac
+done
+
+shift $((OPTIND-1))
+
+
+####create Trees
+if ((addHHTagger)); then
+    python Limits/python/createReducedTrees.py -n $ntup -t $training  -o $outTag -a
+else
+    python Limits/python/createReducedTrees.py -n $ntup -t $training  -o $outTag 
+fi
+    
+####transform MVA output
+python transformMVAOutput.py -i '/mnt/t3nfs01/data01/shome/micheli/HHbbgg_ETH_devel/outfiles/'$outTag'/Total_preselection_diffNaming.root'
+
