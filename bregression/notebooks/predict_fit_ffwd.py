be12476af3100db08cb1617252045e816e1f1cbf
Nadezda <nadezda.chernyavskaya@gmail.com>
2018-01-16 14:25:42 +0100

    preparing scripts for analyzing NN output result

---
+++ bregression/notebooks/predict_fit_ffwd.py
@@ -0,0 +1,96 @@
+# # Characterize FFWD NN output
+#load libs
+import keras.models
+import os
+import bregnn.io as io
+import matplotlib.pyplot as plt
+import sys
+from optparse import OptionParser, make_option
+
+
+parser = OptionParser(option_list=[
+    make_option("--training",type='string',dest="training",default='mse'),
+])
+
+## parse options
+(options, args) = parser.parse_args()
+input_trainings = options.training.split(',')
+
+# ## Read test data and model
+# load data
+base_dir = '/scratch/snx3000/musella/bregression'
+data = io.read_data(base_dir+'/ttbar_unweighted_full80M_selected_test.hd5', columns = None )
+
+for idx,name in enumerate(input_trainings):
+  # list all model files in the training folder
+  #target = '/users/musella/jupyter/bregression/hybrid_cfg'
+  target='/users/nchernya/HHbbgg_ETH/bregression/notebooks/'+input_trainings[idx]
+  models = get_ipython().getoutput('ls -t $target/*.hdf5')
+  models
+
+  # read training configuration
+  import json
+  with open('%s/config.json' % target) as fin:
+      config = json.loads(fin.read())
+  config
+
+  # ## Compute predictions
+  
+  features = config['options']['features'].split(',')
+
+  X = data[features].values
+  y = (data['Jet_mcPt']/data['Jet_pt']).values.reshape(-1,1)
+  y_hbb = (data['Jet_mcPt']/data['Jet_pt_reg']).values.reshape(-1,1)
+  
+  model = keras.models.load_model(models[0],compile=False)
+  
+  y_pred = model.predict(X)
+  
+  # *Note*: the target is typically normalized in the training y = (y-mu)/sigma
+  # ## Convert raw prediction into actual scale and resolution estimation
+
+  
+  if y_pred.shape[1] == 1: # with one output we only have a scale correction
+      corr = y_pred
+      res = None
+  elif y_pred.shape[1] == 2: # with two outputs first is mean and second is sigma
+      corr = y_pred[:,0]
+      res = y_pred[:,1]
+  elif y_pred.shape[1] == 3: # assume that 3 outputs are mean + 2 quantile
+      corr = y_pred[:,0]
+      res = 0.5*(y_pred[:,2] - y_pred[:,1])
+
+  # normalize back to energy scale
+  if config['options']['normalize_target']:
+      corr *= config['y_std']
+      corr += config['y_mean']
+  
+      if res is not None:
+          res *= config['y_std']
+  
+  # errors vector
+  if y_pred.shape[1] > 1:
+      err = y[:,0]-corr
+  else:
+      err = y-corr
+  
+  # ## Characterize the outcome
+
+  print(y.mean(),y.std())
+  
+  me = err.mean()
+  std = err.std()
+  mse = np.square(err).mean()
+  mae = np.abs(err).mean()
+  print('test me ', me, ' std ', std, ' norm std ', std/config['y_std'], ' norm mse ', mse/config['y_std']**2, ' norm mae ', mae / config['y_std'])
+
+  _,bins,_ = plt.hist(y[:,0],bins=100,range=[0.5,2],label='target');
+  plt.hist(y_hbb[:,0],bins=bins,alpha=0.5,label='HIG-17-009');
+  plt.hist(y_pred[:,0],bins=bins,alpha=0.5,label='prediction NN');
+  plt.xlabel('$p_T^{true} / p_T^{reco}$')
+  plt.legend(loc = 'upper right')
+  plt.savefig('/users/nchernya//HHbbgg_ETH/bregression/plots/'+input_trainings[idx]+'/Pasquale_pT_true_reco_Hig_17_009.pdf')
+  plt.clf()
+
+sys.path.insert(0, '/users/nchernya/HHbbgg_ETH/bregression/python/')
+import plotting_utils
