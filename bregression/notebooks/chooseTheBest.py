17b1e846c3a78c8ad7ef4700f605299de881626b
Nadezda <nadezda.chernyavskaya@cern.ch>
2018-01-25 11:32:41 +0100

    brining back old chooseTheBest for scikit

--- bregression/notebooks/chooseTheBest.py
+++ bregression/notebooks/chooseTheBest.py
@@ -21,16 +21,19 @@ path = '/mnt/t3nfs01/data01/shome/nchernya//HHbbgg_ETH_devel/bregression/fitResu
 #pt_regions_names = '(Jet_mcPt>0),(Jet_mcPt<100),(Jet_mcPt>=100 & Jet_mcPt<300),(Jet_mcPt>=300 & Jet_mcPt<400),(Jet_mcPt>=400 & Jet_mcPt<600),(Jet_mcPt>=600)'.split(",")
 #pt_regions_names = '(Jet_mcPt>0),(Jet_mcPt<100),(Jet_mcPt>=100 & Jet_mcPt<150),(Jet_mcPt>=150 & Jet_mcPt<200),(Jet_mcPt>=200 & Jet_mcPt<250),(Jet_mcPt>=250 & Jet_mcPt<300),(Jet_mcPt>=300 & Jet_mcPt<400),(Jet_mcPt>=400 & Jet_mcPt<600),(Jet_mcPt>=600)'.split(",")
 pt_regions_names = '(Jet_mcPt>0),(Jet_mcPt<60),(Jet_mcPt>=60 & Jet_mcPt<100),(Jet_mcPt>=100 & Jet_mcPt<150),(Jet_mcPt>=150 & Jet_mcPt<200),(Jet_mcPt>=200 & Jet_mcPt<250),(Jet_mcPt>=250 & Jet_mcPt<300),(Jet_mcPt>=300 & Jet_mcPt<400),(Jet_mcPt>=400 & Jet_mcPt<600),(Jet_mcPt>=600)'.split(",")
-eta_regions_names = '|Jet_eta|<0.5,|Jet_eta|>=0.5 & |Jet_eta|<1.0,|Jet_eta|>=1.0 & |Jet_eta|<1.5,|Jet_eta|>=1.5 & |Jet_eta|<2.0,|Jet_eta|>=2.0'.split(",")
+#eta_regions_names = '|Jet_eta|<0.5,|Jet_eta|>=0.5 & |Jet_eta|<1.0,|Jet_eta|>=1.0 & |Jet_eta|<1.5,|Jet_eta|>=1.5 & |Jet_eta|<2.0,|Jet_eta|>=2.0'.split(",")
+eta_regions_names = '|Jet_eta|<0.5,|Jet_eta|>=0.5 & |Jet_eta|<0.75,|Jet_eta|>=0.75 & |Jet_eta|<1.0,|Jet_eta|>=1.0 & |Jet_eta|<1.25,|Jet_eta|>=1.25 & |Jet_eta|<1.5,|Jet_eta|>=1.5 & |Jet_eta|<1.75,|Jet_eta|>=1.75 & |Jet_eta|<2.0,|Jet_eta|>=2.0 & |Jet_eta|<2.25,|Jet_eta|>=2.25'.split(",")
 #labels=['Caterina','XGb w weights','XGb wo weights', 'only Cat variables', 'only Cat var, target pt','only Cat var, target pt, old par','quantile 0-4,20-70']
 #labels=['HIG-16-044','no Regression','MSE','quantile 0.4','high pT']
-labels=['HIG-16-044','no Regression','MSE','high pT']
+#labels=['HIG-16-044','no Regression','MSE','high pT']
+#labels=['HIG-17-009','no Regression','high pT']
+labels=['HIG-17-009','no Regression','high pT']
 #which = 'QuantileMinMaxLinear04'
 #saveTag = 'Subsample09_fine_unweighted_'
 #saveTag = 'Cat_noreg_mse'
 #saveTag = 'Cat_noreg_quantile'
 #saveTag= 'twoquantile'
-saveTag='CatnoregMSEHighPt'
+saveTag='Catnoreg_semilep'
 if testfile=='ZHbbll' : 
 	which = saveTag+testfile
 	sample = testfile
@@ -44,7 +47,8 @@ num_pt=len(pt_regions_names)
 num_eta=len(eta_regions_names)
 #pt_region=[-500,0,100,300,400,600,1200]
 pt_region=[-500,0,60,100,150,200,250,300,400,600,1200]
-eta_region=[0.,0.5,1.,1.5,2,2.5]
+#eta_region=[0.,0.5,1.,1.5,2,2.5]
+eta_region=[0.,0.5,0.75,1.,1.25,1.5,1.75,2,2.25,2.5]
 pt_region = [ pt_region[i]+(pt_region[i+1]-pt_region[i])/2. for i in range(0,len(pt_region)-1)]
 eta_region = [ eta_region[i]+(eta_region[i+1]-eta_region[i])/2. for i in range(0,len(eta_region)-1)]
 
