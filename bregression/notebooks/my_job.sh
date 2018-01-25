b24a04245b1cfa5862c318dd470b08756a489648
Nadezda <nadezda.chernyavskaya@gmail.com>
2018-01-25 14:12:39 +0100

    Adding my_job file needed to run NN

---
+++ bregression/notebooks/my_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --job-name=job_name
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nadezda.chernyavskaya@cern.ch
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH -A d78
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export CRAY_CUDA_MPS=1
+
+source $HOME/env.sh
+
+cd $HOME/jupyter/bregression
+
+srun $@
