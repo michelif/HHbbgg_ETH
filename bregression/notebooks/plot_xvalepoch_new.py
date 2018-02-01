71a897c85d1f181c619ef4abdc60c418967deae4
Nadezda <nadezda.chernyavskaya@gmail.com>
2018-02-01 17:30:39 +0100

    plotting xval epochs

---
+++ bregression/notebooks/plot_xvalepoch_new.py
@@ -0,0 +1,63 @@
+# # Characterize FFWD NN output
+#load libs
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import json
+from optparse import OptionParser, make_option
+sys.path.insert(0, '/users/nchernya/HHbbgg_ETH/bregression/python/')
+import plotting_utils as plotting
+import pprint
+
+
+parser = OptionParser(option_list=[
+    make_option("--training",type='string',dest="training",default='HybridLossRaw'),
+    make_option("--inp-dir",type='string',dest="inp_dir",default='/users/nchernya/HHbbgg_ETH/bregression/notebooks/'),
+    make_option("--inp-file",type='string',dest='inp_file',default='metrics'),
+    make_option("--out-dir",type='string',dest="out_dir",default='/users/nchernya//HHbbgg_ETH/bregression/plots/NN_epochs/'),
+    make_option("--metrics",type='string',dest="metrics",default='loss,mae0,mse0,r2_score0'),
+])
+
+colors=['r','b','g']
+## parse options
+(options, args) = parser.parse_args()
+input_trainings = options.training.split(',')
+input_metrics = options.metrics.split(',')
+
+nxval=5
+
+#for each metric we plot it for train and validation sample vs epoch 
+dictVar={}
+all_means=[]
+all_stds=[]
+for idx_nn,nn in enumerate(input_trainings): 
+    data_frames=[]
+    for xval in range(0,nxval):
+       # load data from metrics.csv file
+        tmp_data = pd.read_csv('%s/%s/%s_kfold%i.csv'%(options.inp_dir,nn,options.inp_file,xval))
+        data_frames.append(tmp_data)
+    if idx_nn==0 :  
+        for i,column in enumerate(data_frames[0].columns.values.tolist()):
+            dictVar[column] = i
+    mean = np.mean( np.array( [ data_frames[i].as_matrix() for i in range(0,xval)] ), axis=0 )
+    mean2 = np.mean( np.array( [ data_frames[i].as_matrix() for i in range(0,xval)] )**2, axis=0 )
+    std = np.sqrt(mean2 - mean**2)
+    all_means.append(mean)
+    all_stds.append(std)
+    #plot results
+for idx,met in enumerate(input_metrics):
+    for idx_nn,nn in enumerate(input_trainings): 
+        mean = all_means[idx_nn]
+        std = all_stds[idx_nn]
+        plt.plot(mean[:,dictVar['epoch']],mean[:,dictVar['%s'%met]],linestyle='-',label='%s Training'%(nn.split('HybridLoss')[1]),color=colors[idx_nn])
+        plt.scatter(mean[:,dictVar['epoch']],mean[:,dictVar['val_%s'%met]],label='%s Validation'%(nn.split('HybridLoss')[1]),color=colors[idx_nn])
+        plt.fill_between(mean[:,dictVar['epoch']],mean[:,dictVar['val_%s'%met]]-std[:,dictVar['val_%s'%met]],mean[:,dictVar['val_%s'%met]]+std[:,dictVar['val_%s'%met]],alpha=0.2,label='%s Validation std'%(nn.split('HybridLoss')[1]),color=colors[idx_nn])
+    plt.xlabel('# epochs')
+    plt.ylabel('%s'%met)
+    plt.legend()
+    pltsavename = options.out_dir+'/xval/loss_vs_epoch_%s'%(met)
+    plt.savefig(pltsavename+'.png')
+    plt.savefig(pltsavename+'.pdf')
+    plt.clf()
+  
